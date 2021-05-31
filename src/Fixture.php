30b8fd33e25a3683485bf0894b94df1b039413e3
Vaidas Bagdonas <Gyvastis@users.noreply.github.com>
2021-05-31 16:00:24 +0200

    Update Fixture.php

--- src/Fixture.php
+++ src/Fixture.php
@@ -3,13 +3,11 @@
 namespace PhpArsenal\SymfonyMongoTestingFoundation;
 
 use PhpArsenal\SymfonyMongoTestingFoundation\Traits\FakerTrait;
-use PhpArsenal\SymfonyMongoTestingFoundation\Traits\IdGeneratorTrait;
 use Doctrine\Persistence\ObjectManager;
 
 abstract class Fixture extends \Doctrine\Bundle\FixturesBundle\Fixture
 {
     use FakerTrait;
-    use IdGeneratorTrait;
 
     public function save(ObjectManager $manager, $object): void
     {
